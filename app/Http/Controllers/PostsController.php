28675057fb64231b1468a146f9cc5c193ff34504
unknown <mdmahaburrahmannadim@gmail.com>
2022-12-22 20:52:38 +0600

    working with db with laravel buildin fn

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 // to create a resource controller we need to give this command (php artisan make:controller controllerName --resource)
 class PostsController extends Controller
 {
@@ -13,7 +15,25 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('blog.index');
+    //   quary bilder
+    // $posts = DB::statement('SELECT * FROM posts'); it's mean that the DB is queriable
+    // $posts = DB::select('SELECT * FROM posts WHERE id =1')
+    // $posts = DB::select('SELECT * FROM posts WHERE id = ?',[2])single parameter binding
+    // $posts = DB::select('SELECT * FROM posts WHERE id = :id',['id'=>1])multiple parameter binding
+
+    $posts = DB::insert('INSERT INTO posts(title,excerpt,body,image_path,is_published,min_to_read) VALUES(?,?,?,?,?,?)',[
+        'THIS IS last post',
+        'I like this course',
+        'I love this course',
+        'example.png',
+        true,
+        1
+    ])
+    
+    ;
+    // var_dump($posts);
+    dd($posts);
+
     }
 
     /**
