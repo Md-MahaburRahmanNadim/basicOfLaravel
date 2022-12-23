6a3350a168d92a69b77e8b79fd51586bc1c0b21b
unknown <mdmahaburrahmannadim@gmail.com>
2022-12-23 10:25:20 +0600

    learning blade control structure like if,foreach,forelse,unless which is the reverse of if

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -7,11 +7,36 @@
     <title>Blog</title>
 </head>
 <body>
-    @foreach ($posts as $post)
-        {{-- @dump($post) --}}
+    {{-- like foreach loop --}}
+    {{-- @forelse ($posts as $post)
         <p>{{ $post->title }}</p>
-        <p>{{ $post->body }}</p>
-    @endforeach
+    @empty
+        <p>post is not found</p>
+    @endforelse --}}
+    @if (count($posts) >100){
+        <h1>We have more than 100 posts</h1>
+    }
+    @elseif(count($post)<100)
+        <h1>We have lass then 100 hundrad post</h1>
+    
+        
+    @else
+        <h1>no post</h1>
+    @endif
+    {{-- reverse then the if  --}}
+    @unless ($posts)
+        <h1>post has been added</h1>
+        
+    @endunless
+
+    {{-- some hidden property in the foreach or forelse loop--}}
+    @forelse ($posts as $post)
+        {{-- @dump($loop) this will show you all of the hidden property in you loop object --}}
+        {{-- {{ $loop->first }} it's give us the bool output --}}
+        {{-- {{ $loop->last}} it's also give us the bool output --}}
+    @empty
+        
+    @endforelse
     
 </body>
 </html>
