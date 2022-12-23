428ebac112121ad6702d4074bd89dda8244ee3c0
unknown <mdmahaburrahmannadim@gmail.com>
2022-12-23 15:00:13 +0600

    modifying some default value of the elequent or db

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    // if our Model can't find the posts table anyway then we have to bind it like below
+    // protected $table = 'posts';
+    // protected $primaryKey = 'title';
+    // protected $timestamps = false;
+    // protected $dateTime = 'U';
+    // protected $connection = 'sqllite';
+    // // assigining default value for a column
+    // protected $attributes = [
+    //     'title'=> 'This is post title'
+    // ];
+
 }
